4c05516a59c1b18b0cbdc511e9142a2802083bfb
nyhryan <nyhryan99102@gmail.com>
2023-12-18 20:03:00 +0900

    enlargeEmoji.js supports both Discord custom emojis and Unicode emojis

--- events/enlargeEmoji.js
+++ events/enlargeEmoji.js
@@ -1,38 +1,84 @@
 const { AttachmentBuilder, Events } = require('discord.js');
-const Jimp = require('jimp');
 const { GifFrame, GifUtil, GifCodec, BitmapImage } = require('gifwrap');
+const { wrap } = require('../helper/helper');
+const { parse } = require('node-html-parser');
+const Jimp = require('jimp');
 const axios = require('axios');
+const emojiRegex = require('emoji-regex');
+const emojiJson = require('emojilib');
 require('dotenv').config();
 
 module.exports = {
 	name: Events.MessageCreate,
-	async execute(interaction) {
-		// gets emoji string like <(a:)name:id> from message
-		const emojiFromContent = interaction.content.match(/^<a?:\w+:\d+>$/g);
-		// gets emoji id from emoji string
-		const emojiId = emojiFromContent !== null ? emojiFromContent[0].match(/\d{2,}/g)[0] : null;
+	async execute(message) {
+		const _emojiRegex = emojiRegex();
+		const resultUnicodeEmojis = message.content.match(_emojiRegex);
 
-		if (emojiId === null) {
+		// gets emoji string like <(a:)name:id> from message
+		const resultDiscordEmoji = message.content.match(/^<a?:\w+:\d+>$/g);
+
+		if (resultDiscordEmoji === null && resultUnicodeEmojis === null) {
+			return;
+		}
+		else if (resultDiscordEmoji === null && resultUnicodeEmojis !== null && resultUnicodeEmojis.length > 1) {
+			return;
+		}
+		// check if message.content only contains a single emoji
+		const emojiObj = {
+			animated: false,
+			id: '',
+			name: '',
+			src: '',
+		};
+
+		if (resultUnicodeEmojis === null && resultDiscordEmoji !== null) {
+			emojiObj.animated = resultDiscordEmoji[0].match(/^<a:/g) !== null ? true : false;
+			emojiObj.id = resultDiscordEmoji[0].match(/\d{2,}/g)[0];
+			emojiObj.name = resultDiscordEmoji[0].match(/:\w+:/g)[0].replace(/:/g, '');
+			emojiObj.src = `https://cdn.discordapp.com/emojis/${emojiObj.id}`;
+		}
+		else {
+			emojiObj.animated = false;
+			emojiObj.name = emojiJson[resultUnicodeEmojis[0]][0].replace(/[_\s]/g, '-');
+
+			// get emoji image from emojipedia
+			const [response, responseErr] = await wrap(axios.get(`https://emojipedia.org/microsoft-3D-fluent/15.0/${emojiObj.name}`));
+			if (responseErr) {
+				console.error(responseErr.code);
+				return;
+			}
+			// get the actual image file from the html page of emojipedia
+			emojiObj.src = parse(response.data).querySelector('main section > div > div > div > div > img').getAttribute('src');
+		}
+
+		const [frameImage, frameImageErr] = await wrap(Jimp.read('./assets/frame.png'));
+		if (frameImageErr) {
+			console.error(frameImageErr);
 			return;
 		}
 
-		const emojiName = emojiFromContent[0].match(/:\w+:/g)[0].replace(/:/g, '');
-		console.log(`Emoji name: ${emojiName}`);
-		console.log(`Emoji id: ${emojiId}`);
+		const attachment = new AttachmentBuilder('');
+		// if user sent an animated emoji
+		if (emojiObj.animated) {
+			// Get emoji gif from discord cdn
+			const [response, responseErr] = await wrap(axios.get(emojiObj.src, { responseType: 'arraybuffer' }));
+			if (responseErr) {
+				console.error(responseErr);
+				return;
+			}
 
-		const isEmojiAnimated = emojiFromContent[0].match(/^<a:/g) !== null ? true : false;
-		const frameImage = await Jimp.read('./assets/frame.png');
-
-
-		if (isEmojiAnimated) {
-			const res = await axios.get(`https://cdn.discordapp.com/emojis/${emojiId}.gif`, { responseType: 'arraybuffer' });
-
-			const emojiGif = await GifUtil.read(res.data);
+			// Read emoji gif into gifwrap Gif object
 			const frames = [];
+			const [emojiGif, emojiGifErr] = await wrap(GifUtil.read(response.data));
+			if (emojiGifErr) {
+				console.error(emojiGifErr);
+				return;
+			}
+			// Overlay frame image on each frame of emoji gif
 			for (const frame of emojiGif.frames) {
-				const emojiGifFrame = GifUtil.copyAsJimp(Jimp, frame);
-				emojiGifFrame.resize(128, 128);
-				const compositedFrame = frameImage.clone().composite(emojiGifFrame, 7, 27);
+				const currentFrameAsJimp = GifUtil.copyAsJimp(Jimp, frame);
+				currentFrameAsJimp.resize(128, 128);
+				const compositedFrame = frameImage.clone().composite(currentFrameAsJimp, 7, 27);
 
 				const gifFrame = new GifFrame(
 					new BitmapImage(compositedFrame.bitmap),
@@ -43,35 +89,58 @@ module.exports = {
 				frames.push(gifFrame);
 			}
 
+
+			// Encode gif
 			const codec = new GifCodec();
 			GifUtil.quantizeDekker(frames);
-			const encodedGif = await codec.encodeGif(frames, { loop: 0 });
-			const attachment = new AttachmentBuilder(
-				encodedGif.buffer,
-				{ name: 'emoji.gif' },
-			);
+			const [encodedGif, encodedGifErr] = await wrap(codec.encodeGif(frames, { loop: 0 }));
+			if (encodedGifErr) {
+				console.error(encodedGifErr);
+				return;
+			}
 
-			const channel = interaction.client.channels.cache.get(interaction.channelId);
-			channel.send({
-				files: [attachment],
-			});
+			// Set attachment
+			attachment.attachment = encodedGif.buffer;
+			attachment.name = `${emojiObj.name}.gif`;
 		}
-		else {
-			const emojiImg = await Jimp.read(`https://cdn.discordapp.com/emojis/${emojiId}.png`);
-			emojiImg.resize(128, 128);
-			frameImage.composite(emojiImg, 7, 27);
-			frameImage.getBuffer(Jimp.MIME_PNG, (err, buffer) => {
-				if (err) throw err;
-				const attachment = new AttachmentBuilder(
-					buffer,
-					{ name: 'emoji.png' },
-				);
+		// if it's not animated, it's a non-animated discord custom emoji or an unicode emoji
+		else if (!emojiObj.animated) {
+			const [emojiPng, emojiPngErr] = await wrap(Jimp.read(emojiObj.src));
+			if (emojiPngErr) {
+				console.error(emojiPngErr);
+				return;
+			}
 
-				const channel = interaction.client.channels.cache.get(interaction.channelId);
-				channel.send({
-					files: [attachment],
-				});
-			});
+			emojiPng.resize(128, 128);
+			frameImage.composite(emojiPng, 7, 27);
+
+			// Get png buffer
+			const [pngBuffer, pngBufferErr] = await wrap(frameImage.getBufferAsync(Jimp.MIME_PNG));
+			if (pngBufferErr) {
+				console.error(pngBufferErr);
+				return;
+			}
+
+			// Set attachment
+			attachment.attachment = pngBuffer;
+			attachment.name = `${emojiObj.name}.png`;
+		}
+
+		// Delete original message
+		const channel = message.client.channels.cache.get(message.channelId);
+		const [, deleteErr] = await wrap(message.delete());
+		if (deleteErr) {
+			console.error(deleteErr);
+			return;
+		}
+
+		// Send enlarged emoji
+		const [, sendErr] = await wrap(channel.send({
+			files: [attachment],
+		}));
+		if (sendErr) {
+			console.error(sendErr);
+			return;
 		}
 	},
 };
