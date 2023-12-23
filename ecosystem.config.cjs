ff5ec5f193c71f6f14736e8134d82623ba551f4c
ATAI <nyhryan99102@gmail.com>
2023-12-24 00:34:27 +0900

    Add cwd path to ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,6 +2,7 @@ module.exports = {
 	apps : [{
 		name   : 'Discord Bot',
 		script : './dist/index.js',
+		cwd: '/home/atai/actions-runner/_work/atai-bot-2023/atai-bot-2023/',
 		interpreter: '~/.bun/bin/bun',
 		instances: 'max',
 		exec_mode: 'cluster',
