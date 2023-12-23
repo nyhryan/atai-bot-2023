f520ac96787a890a1ce6c1af78d71bfdd6355caf
ATAI <nyhryan99102@gmail.com>
2023-12-24 00:38:42 +0900

    Update interpreter path in ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
 		name   : 'Discord Bot',
 		script : './dist/index.js',
 		cwd: '/home/atai/actions-runner/_work/atai-bot-2023/atai-bot-2023/',
-		interpreter: '~/.bun/bin/bun',
+		interpreter: '/home/atai/.bun/bin/bun',
 		instances: 'max',
 		exec_mode: 'cluster',
 	}],
