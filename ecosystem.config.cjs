ebe011d7c458d8bffcf4cda4619021fd65c0a59c
ATAI <nyhryan99102@gmail.com>
2023-12-24 00:51:57 +0900

    Update build configuration and dependencies

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
 	apps : [{
 		name   : 'Discord Bot',
-		script : './dist/index.js',
+		script : './src/index.ts',
 		cwd: '/home/atai/actions-runner/_work/atai-bot-2023/atai-bot-2023/',
 		interpreter: '/home/atai/.bun/bin/bun',
 		instances: 'max',
