f4a9cd5130e534920b87168da7f9680902e893e8
ATAI <nyhryan99102@gmail.com>
2023-12-24 02:22:17 +0900

    update pm2 config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,7 +4,5 @@ module.exports = {
 		script : './src/index.ts',
 		interpreter: '/home/atai/.bun/bin/bun',
 		cwd: '/home/atai/actions-runner/_work/atai-bot-2023/atai-bot-2023/',
-		instances: 'max',
-		exec_mode: 'cluster',
 	}],
 };
