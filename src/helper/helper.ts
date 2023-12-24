f30c3477da2bbe0ec22461612f39d833b27224b7
ATAI <nyhryan99102@gmail.com>
2023-12-24 11:48:23 +0900

    Refactor wrap function in helper.ts

--- src/helper/helper.ts
+++ src/helper/helper.ts
@@ -9,21 +9,16 @@ import { fileURLToPath } from 'url';
  * @returns A promise that resolves to a `[value, reason]` array.
  */
 export const wrap = async (promise: Promise<any>) => {
-	return Promise.allSettled([promise])
-		.then(([result]) => {
-			if (result.status === 'fulfilled') {
-				return [result.value, undefined];
-			}
-			else {
-				return [undefined, result.reason];
-			}
-		});
+	const [result] = await Promise.allSettled([promise]);
+	const value = result.status === 'fulfilled' ? result.value : undefined;
+	const reason = result.status === 'rejected' ? result.reason : undefined;
+	return [value, reason];
 };
 
 export const getDirName = (url: string | URL) => path.dirname(fileURLToPath(url));
 
 export const getCacheDir = (() => {
-	const cacheDir = './.cache';
+	const cacheDir = './.emoji_cache';
 
 	let prom: Promise<string> | undefined;
 	return () => prom = (prom || (async () => {
