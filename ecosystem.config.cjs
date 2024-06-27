51a37c6bd18c2e212a9646910e8a5ddfa9d507fc
ATAI <nyhryan99102@gmail.com>
2024-06-27 20:41:15 +0900

    refixed pm2 script

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
 	apps : [{
 		name   : 'Discord Bot',
 		script : './dist/index.js',
-		interpreter: '$HOME/.bun/bin/bun',
-		cwd: '$HOME/actions-runner/_work/atai-bot-2023/atai-bot-2023/',
+		interpreter: '/home/atai/.bun/bin/bun',
+		cwd: '/home/atai/actions-runner/_work/atai-bot-2023/atai-bot-2023/',
 	}],
 };
